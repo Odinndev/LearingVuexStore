bb2eadc8e8fabd188fb868cbfec238b16bed2d69
Óðinn Kristjánsson <39589991+odinnmk@users.noreply.github.com>
2020-10-19 12:13:33 +0100

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <div class="container">
+      <div class="">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-body">
+              <h1>Hello</h1>
+              <P>{{ stateData }}</P>
+
+              <router-link to="/update" class="btn btn-outline-secondary">
+                Next
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      stateData: "",
+      message: "Hallow World!",
+    };
+  },
+
+  mounted() {
+    this.stateData = this.$store.getters.getMessage;
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
